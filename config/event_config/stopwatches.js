5af3ea54d92d83d49eb0d0804083f75418ea65a4
Wipeout111 <78170074+Wipeout111@users.noreply.github.com>
2021-01-30 19:40:04 -0500

    Update stopwatches.js

--- config/event_config/stopwatches.js
+++ config/event_config/stopwatches.js
@@ -20,4 +20,20 @@ module.exports = [
             'spawn %name%',
         ]
     },
+     {
+        id: 'DintyCode',//the id that you'll use for all your command blocks
+        friendlyName: 'WipeoutTest',//friendly name that the challenge is known by
+        discordResultMessages: [
+            {
+                channelID: '804019022055604285',//the id of the channel to post to. channelName overrides this, but good to have as a backup
+                channelName: 'challenge-%name%',//the name of a channel to post to, placeholders are allowed. if this channel cannot be found then it will fall back to the specified id
+                content: '%name% completed challenge %friendlyName% with a time of %time%'//message content
+            }
+        ],
+        minecraftResultCommands: [//minecraft commands to run via the discord console
+            'bc %name% just completed the challenge %friendlyName% with a time of %time%',
+            'spawn %name%',
+        ]
+     },
+  
 ]
