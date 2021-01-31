d89ac946f73f226b9b53cf029bebb14f25f75c2b
DB8888 <67452089+DB8888@users.noreply.github.com>
2021-01-31 00:48:26 +0000

    comments

--- config/event_config/stopwatches.js
+++ config/event_config/stopwatches.js
@@ -2,6 +2,8 @@ module.exports = [
     //Config for stopwatches, comma separated
     //anything within [] can have more than one value, but don't forget to separate with a comma
     //you can create multiple stopwatches by copy-pasting the below object
+    //available placeholders (surround with %):
+    //id, friendlyName, time, name (playername)
     //command block configuration:
     //start: /discord bcast #bot-chat stopwatch <id> start @p
     //end: /discord bcast #bot-chat stopwatch <id> stop @p
@@ -20,20 +22,5 @@ module.exports = [
             'spawn %name%',
         ]
     },
-     {
-        id: 'DintyCode',//the id that you'll use for all your command blocks
-        friendlyName: 'WipeoutTest',//friendly name that the challenge is known by
-        discordResultMessages: [
-            {
-                channelID: '804019022055604285',//the id of the channel to post to. channelName overrides this, but good to have as a backup
-                channelName: 'challenge-%name%',//the name of a channel to post to, placeholders are allowed. if this channel cannot be found then it will fall back to the specified id
-                content: '%name% completed challenge %friendlyName% with a time of %time%'//message content
-            }
-        ],
-        minecraftResultCommands: [//minecraft commands to run via the discord console
-            'bc %name% just completed the challenge %friendlyName% with a time of %time%',
-            'spawn %name%',
-        ]
-     },
   
 ]
